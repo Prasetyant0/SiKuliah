7b51acca6da4e6f95849e1a8cf8c36173be07550
Prasetyanto <powerbet31@gmail.com>
2024-11-01 01:58:15 +0700

    Menampilkan data materi pada GridView

--- app/src/main/java/com/example/apksiswa1526/Materi.kt
+++ app/src/main/java/com/example/apksiswa1526/Materi.kt
@@ -1,6 +1,7 @@
 package com.example.apksiswa1526
 
 import android.os.Bundle
+import android.widget.GridView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +10,23 @@ import androidx.core.view.WindowInsetsCompat
 class Materi : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.materi)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val gv_materi:GridView = findViewById(R.id.gv_materi)
+
+        var juduls:MutableList<String> = mutableListOf()
+        juduls.add("Pengenalan Bahasa Kotlin Amikom")
+        juduls.add("Pindah Halaman Dengan Intent")
+        juduls.add("Menampilkan Data Statis")
+        juduls.add("Menampilkan Data Dinamis")
+
+        var namas:MutableList<String> = mutableListOf()
+        namas.add("Arif Aminudin")
+        namas.add("Bambang Pamungkas")
+        namas.add("Cintya Dewi")
+        namas.add("Doni Saputra")
+
+        val perulangandata = Materi_item(this, juduls, namas)
+        gv_materi.adapter = perulangandata
     }
 }
