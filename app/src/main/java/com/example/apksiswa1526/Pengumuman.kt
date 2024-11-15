94c6e9dea8f4898237fe07c95ed7346fba157cac
Prasetyanto <powerbet31@gmail.com>
2024-11-16 06:35:17 +0700

    Config Volley

--- app/src/main/java/com/example/apksiswa1526/Pengumuman.kt
+++ app/src/main/java/com/example/apksiswa1526/Pengumuman.kt
@@ -20,28 +20,7 @@ class Pengumuman : AppCompatActivity() {
 
         //kita buat data juduls pengumuman
         var juduls:MutableList<String> = mutableListOf()
-        juduls.add("pengumuman amikom 1")
-        juduls.add("pengumuman amikom 2")
-        juduls.add("pengumuman amikom 3")
-        juduls.add("pengumuman amikom 4")
-        juduls.add("pengumuman amikom 5")
-        juduls.add("pengumuman amikom 6")
-        juduls.add("pengumuman amikom 7")
-        juduls.add("pengumuman amikom 8")
-        juduls.add("pengumuman amikom 9")
-        juduls.add("pengumuman amikom 10")
-
         var tanggals:MutableList<String> = mutableListOf()
-        tanggals.add("1 Januari 2024")
-        tanggals.add("1 Februari 2024")
-        tanggals.add("1 Maret 2024")
-        tanggals.add("1 April 2024")
-        tanggals.add("1 Mei 2024")
-        tanggals.add("1 Juni 2024")
-        tanggals.add("1 Juli 2024")
-        tanggals.add("1 Agustus 2024")
-        tanggals.add("1 September 2024")
-        tanggals.add("1 Oktober 2024")
 
         var lv_pengumuman:ListView = findViewById(R.id.lv_pengumuman)
         var perulangandata = Pengumuman_item(this, juduls, tanggals)
