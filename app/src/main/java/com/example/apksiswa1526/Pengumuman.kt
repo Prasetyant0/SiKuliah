5d9edc82d73a93e82e38535abb46f0f11313081e
Prasetyanto <powerbet31@gmail.com>
2024-11-16 07:09:54 +0700

    Config API in Pengumuman

--- app/src/main/java/com/example/apksiswa1526/Pengumuman.kt
+++ app/src/main/java/com/example/apksiswa1526/Pengumuman.kt
@@ -1,11 +1,17 @@
 package com.example.apksiswa1526
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.ListView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONObject
 
 class Pengumuman : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,7 +29,35 @@ class Pengumuman : AppCompatActivity() {
         var tanggals:MutableList<String> = mutableListOf()
 
         var lv_pengumuman:ListView = findViewById(R.id.lv_pengumuman)
-        var perulangandata = Pengumuman_item(this, juduls, tanggals)
-        lv_pengumuman.adapter = perulangandata
+        val minta = Volley.newRequestQueue(this)
+        val mintaData:StringRequest = object : StringRequest(
+            Request.Method.POST,
+            "http://192.168.1.32/android_amikom/pengumuman_tampil.php",
+            Response.Listener<String> { response->
+                val dataarray = JSONObject(response).getJSONArray("data")
+
+                for (i in 0 until dataarray.length()) {
+                    val jdl = dataarray.getJSONObject(i).getString("judul_pengumuman")
+                    val tgl = dataarray.getJSONObject(i).getString("tanggal_pengumuman")
+
+                    juduls.add(jdl)
+                    tanggals.add(tgl)
+                }
+                var perulangandata = Pengumuman_item(this, juduls, tanggals)
+                lv_pengumuman.adapter = perulangandata
+            },
+            Response.ErrorListener {
+                Log.d("eekror", "bermasalah")
+            }
+        ) {
+            override fun getParams(): MutableMap<String, String>? {
+                val bawaan:MutableMap<String, String> = HashMap()
+                bawaan.put("kode", "amikomoke")
+
+                return bawaan
+            }
+        }
+        minta.add(mintaData)
+
     }
 }
